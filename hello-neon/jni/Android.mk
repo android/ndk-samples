199efd08dea914246ead32b6a3031084d09ab479
David 'Digit' Turner <digit@google.com>
2010-07-13 15:50:46 -0700

    ndk sample hello-neon: use $(call import-module) to access the 'cpufeatures' library
    Change-Id: I29397158de0ccdd89dfe4da72a544fad14a8324e

--- hello-neon/jni/Android.mk
+++ hello-neon/jni/Android.mk
@@ -11,12 +11,10 @@ ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
     LOCAL_SRC_FILES += helloneon-intrinsics.c.neon
 endif
 
-LOCAL_C_INCLUDES := $(NDK_ROOT)/sources/cpufeatures
-
 LOCAL_STATIC_LIBRARIES := cpufeatures
 
 LOCAL_LDLIBS := -llog
 
 include $(BUILD_SHARED_LIBRARY)
 
-include $(NDK_ROOT)/sources/cpufeatures/Android.mk
+$(call import-module,cpufeatures)
