f398e30566fa1c1b97c9224f52cea7ced4c7f8f9
Jeff Brown <jeffbrown@google.com>
2010-09-13 23:57:28 -0700

    Update NDK headers and samples to sync up with looper API changes.
    Change-Id: I7082603bf280888be90dc4df687521aaae9b1d02

--- native-activity/jni/main.c
+++ native-activity/jni/main.c
@@ -236,7 +236,7 @@ void android_main(struct android_app* state) {
         // If not animating, we will block forever waiting for events.
         // If animating, we loop until all events are read, then continue
         // to draw the next frame of animation.
-        while ((ident=ALooper_pollAll(engine.animating ? 0 : -1, &events,
+        while ((ident=ALooper_pollAll(engine.animating ? 0 : -1, NULL, &events,
                 (void**)&source)) >= 0) {
 
             // Process this event.
