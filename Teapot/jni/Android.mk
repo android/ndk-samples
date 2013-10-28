9eddeffdefcfd84860c7100d49f2a2cba2cb4b32
Andrew Hsieh <andrewhsieh@google.com>
2013-10-28 12:03:09 +0800

    Rename modules NativeActivity
    To TeapotNativeActivity and MoreTeapotsNativeActivity, respectively
    
    Change-Id: Iddbb326ef246232e64a9c30138edb55efe86230d

--- Teapot/jni/Android.mk
+++ Teapot/jni/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE    := NativeActivity
+LOCAL_MODULE    := TeapotNativeActivity
 LOCAL_SRC_FILES := TeapotNativeActivity.cpp \
 TeapotRenderer.cpp \
 
--- Teapot/jni/TeapotNativeActivity.cpp
+++ Teapot/jni/TeapotNativeActivity.cpp
@@ -444,7 +444,7 @@ void android_main( android_app* state )
     state->onInputEvent = Engine::HandleInput;
 
 #ifdef USE_NDK_PROFILER
-    monstartup("libNativeActivity.so");
+    monstartup("libTeapotNativeActivity.so");
 #endif
 
     // Prepare to monitor accelerometer
