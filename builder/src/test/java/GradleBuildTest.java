4c903020c2c83bab3612b7214e1a9ff170abe40b
Johan Euphrosine <proppy@google.com>
2015-06-30 17:10:35 -0700

    builder: only build dirty projects

--- builder/src/test/java/GradleBuildTest.java
+++ builder/src/test/java/GradleBuildTest.java
@@ -8,19 +8,85 @@ import org.gradle.tooling.BuildLauncher;
 import org.gradle.tooling.GradleConnector;
 import org.gradle.tooling.ProjectConnection;
 
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.diff.DiffEntry;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+import org.eclipse.jgit.treewalk.TreeWalk;
+import org.eclipse.jgit.treewalk.filter.TreeFilter;
+
 import java.io.File;
 import java.io.FileFilter;
 import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collection;
 
 @RunWith(Parameterized.class)
-public class GradleBuildTest {
-
+public class GradleBuildTest {    
   @Parameters
-  public static File[] data() {
-      return new File("..").listFiles(new FileFilter() {
-	      public boolean accept(File f) { return f.isDirectory() && Arrays.asList(f.list()).containsAll(Arrays.asList("build.gradle", "app")); }
-      });
+  public static Collection<File> data() {
+      ArrayList<File> dirtyProjects = new ArrayList<File>(); 
+      try {
+	  Repository repository = new FileRepositoryBuilder()
+	      .readEnvironment() // scan environment GIT_* variables
+	      .findGitDir() // scan up the file system tree
+	      .build();
+	  RevCommit head = new Git(repository)
+	      .log().call()
+	      .iterator().next();
+	  TreeWalk treeWalk = new TreeWalk(repository);
+	  treeWalk.addTree(head.getTree());
+	  for (RevCommit p : head.getParents()) {
+	      treeWalk.addTree(p.getTree());
+	  }
+	  treeWalk.setRecursive(false);
+	  treeWalk.setFilter(TreeFilter.ANY_DIFF);
+	  while (treeWalk.next()) {
+	      System.err.println("modified:" + treeWalk.getPathString());
+	      File f = new File("../" + treeWalk.getPathString());
+	      if (f.getName() == ".travis.yml") {
+		  System.err.println("travis modified");
+		  return allProjects();
+	      }
+	      if (f.isDirectory() && f.getName() == "builder") {
+		  System.err.println("builder modified");
+		  return allProjects();
+	      }
+	      if (isProject(f)) {
+		  System.err.println("added project:" + f.getName());		  
+		  dirtyProjects.add(f);
+	      } else {
+		  System.err.println("not a project:" + f.getName());
+	      }
+	  }
+      } catch (java.io.IOException e) {
+	  System.err.println("error opening git repository:" + e);
+	  return allProjects();
+      } catch (GitAPIException e) {
+	  System.err.println("error reading git repository log:" + e);
+	  return allProjects();
+      }
+      if (dirtyProjects.size() > 0) {
+	  System.err.println("building modified projects:" + dirtyProjects.toString());	  
+	  return dirtyProjects;
+      }
+      System.err.println("no modified projects");
+      return allProjects();
   }
+
+  private static Collection<File> allProjects() {
+      System.err.println("building all projects");
+      return Arrays.asList(new File("..").listFiles(new FileFilter() {
+	      public boolean accept(File f) { return isProject(f); }
+	  }));
+  }
+
+  private static boolean isProject(File f) {
+      return f.isDirectory() && Arrays.asList(f.list()).containsAll(Arrays.asList("build.gradle", "app"));
+  }
+    
   private File gradleProject;
   public GradleBuildTest(File gradleProject) {
       this.gradleProject = gradleProject;
@@ -32,14 +98,18 @@ public class GradleBuildTest {
       connector.forProjectDirectory(gradleProject);
       ProjectConnection connection = connector.connect();
       try {
-	  // Configure the build
 	  BuildLauncher launcher = connection.newBuild();
-	  launcher.forTasks("build");
 	  launcher.setStandardOutput(System.out);
-	  launcher.setStandardError(System.err);
-	  
-	 // Run the build
+	  launcher.setStandardError(System.err);	  
+
+	  launcher.forTasks("clean");
 	  launcher.run();
+	  
+	  launcher.forTasks("app:lint");
+	  launcher.run();
+
+	  launcher.forTasks("build");
+	  launcher.run();	  
       } finally {
 	  // Clean up
 	  connection.close();
