5b374ec9cc6d2483ff1ee8f5a777bcc5805a5bb6
ggfan <gfan@google.com>
2015-08-21 09:23:15 -0700

    removing unused andoird.log import

--- native-audio/app/src/main/java/com/example/nativeaudio/NativeAudio.java
+++ native-audio/app/src/main/java/com/example/nativeaudio/NativeAudio.java
@@ -22,7 +22,6 @@ import android.content.res.AssetManager;
 import android.media.AudioManager;
 import android.os.Build;
 import android.os.Bundle;
-//import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AdapterView;
