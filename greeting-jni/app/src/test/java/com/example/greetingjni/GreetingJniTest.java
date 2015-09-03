73bc08bc955a33fc2a301780fe98534ea3c4c589
Roberto Carlos Gonzalez Flores <iscrobertogonzalez@gmail.com>
2015-09-03 11:07:25 -0700

    Sample Test
    I’ll modify this test to be more intelligent and to interact with the
    project, this is only to test the JUnit setup.

---
+++ greeting-jni/app/src/test/java/com/example/greetingjni/GreetingJniTest.java
@@ -0,0 +1,13 @@
+package com.example.greetingjni;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class GreetingJniTest {
+
+    @Test
+    public void sampleTest() {
+        assertEquals(2,1+1);
+    }
+}
