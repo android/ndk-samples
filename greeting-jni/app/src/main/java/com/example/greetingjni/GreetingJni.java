7c1ac024ad4f9e58bf6f18f368f830036a966271
Roberto Carlos Gonzalez Flores <iscrobertogonzalez@gmail.com>
2015-09-03 22:38:17 -0700

    Implementing the gretting message in C
    Moving the JNI invocation to a “model” class to leave the UI in its own
    class, this will help me to create some UnitTests.

--- greeting-jni/app/src/main/java/com/example/greetingjni/GreetingJni.java
+++ greeting-jni/app/src/main/java/com/example/greetingjni/GreetingJni.java
@@ -26,6 +26,7 @@ public class GreetingJni extends Activity
 {
     private EditText mEditText;
     private TextView mTextView;
+    private Greeter mGreeter;
 
     /** Called when the activity is first created. */
     @Override
@@ -35,39 +36,15 @@ public class GreetingJni extends Activity
         setContentView(R.layout.greeting);
         mEditText = (EditText) findViewById(R.id.editText);
         mTextView = (TextView) findViewById(R.id.greetingTextView);
+        mGreeter = new Greeter();
     }
 
     public void sendName(View view) {
         /* The text is retrieved by calling a native
-         * function and set into a TextView.
+         * function in Greeter class and set the result into a TextView.
          */
-        mTextView.setText(stringFromJNI(mEditText.getText().toString()));
+        final String greeting = mGreeter.sendName(mEditText.getText().toString());
+        mTextView.setText(greeting);
     }
 
-    /* A native method that is implemented by the
-     * 'greeting-jni' native library, which is packaged
-     * with this application.
-     */
-    public native String  stringFromJNI(String parameter);
-
-    /* This is another native method declaration that is *not*
-     * implemented by 'greeting-jni'. This is simply to show that
-     * you can declare as many native methods in your Java code
-     * as you want, their implementation is searched in the
-     * currently loaded native libraries only the first time
-     * you call them.
-     *
-     * Trying to call this function will result in a
-     * java.lang.UnsatisfiedLinkError exception !
-     */
-    public native String  unimplementedStringFromJNI();
-
-    /* this is used to load the 'greeting-jni' library on application
-     * startup. The library has already been unpacked into
-     * /data/data/com.example.greeting-jni/lib/libgreeting-jni.so at
-     * installation time by the package manager.
-     */
-    static {
-        System.loadLibrary("greeting-jni");
-    }
 }
