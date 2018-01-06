c05adf5ed5c8a71692fbeefe6ed49db1290ae60e
guanghuafan <gfan@google.com>
2018-01-06 13:34:14 -0800

    Cleaning up gles3jni sample, details in: https://github.com/googlesamples/android-ndk/issues/481

--- other-builds/ndkbuild/gles3jni/app/Android.mk
+++ other-builds/ndkbuild/gles3jni/app/Android.mk
@@ -25,9 +25,10 @@ LOCAL_SRC_FILES := $(JNI_SRC_PATH)/gles3jni.cpp    \
 				   $(JNI_SRC_PATH)/RendererES3.cpp
 
 OPENGL_LIB := GLESv3
-#  openGL ES2 platforms, enable the following to use stub
+#  To build for openGL ES2 compatible platforms,
+#  enable the following to use stub
 #   OPENGL_LIB := GLESv2
-#   LOCAL_SRC_FILES += $(JNI_SRC_PATH)/gl3stub_wrapper.c
+#   LOCAL_SRC_FILES += $(JNI_SRC_PATH)/gl3stub.c
 #   LOCAL_CFLAGS += -DDYNAMIC_ES3
 
 LOCAL_LDLIBS    := -llog -l$(OPENGL_LIB) -lEGL
