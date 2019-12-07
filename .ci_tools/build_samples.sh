cffe9c5fa1673ed4f7e5d60458e546fead12ee04
Gerry <ggfan@users.noreply.github.com>
2019-12-07 09:23:01 -0800

    Turn on ci for ndk-samples (#674)

--- .ci_tools/build_samples.sh
+++ .ci_tools/build_samples.sh
@@ -32,7 +32,23 @@ declare projects=(
     san-angeles
     sensor-graph
 #    webp
-    teapots)
+    teapots
+##   ndk-build samples
+    other-builds/ndkbuild/bitmap-plasma
+    other-builds/ndkbuild/gles3jni
+    other-builds/ndkbuild/hello-gl2
+    other-builds/ndkbuild/hello-jni
+    other-builds/ndkbuild/hello-libs
+    other-builds/ndkbuild/hello-neon
+    other-builds/ndkbuild/native-activity
+    other-builds/ndkbuild/native-audio
+    other-builds/ndkbuild/native-codec
+    other-builds/ndkbuild/native-media
+    other-builds/ndkbuild/native-plasma
+    other-builds/ndkbuild/nn_sample
+    other-builds/ndkbuild/san-angeles
+    other-builds/ndkbuild/teapots
+    )
 
 for d in "${projects[@]}"; do 
     pushd ${NDK_SAMPLE_REPO}/${d} >/dev/null
@@ -43,31 +59,50 @@ done
 
 # Check the apks that all get built fine
 declare apks=(
-    audio-echo/app/build/outputs/apk/debug/app-debug.apk
-    bitmap-plasma/app/build/outputs/apk/debug/app-debug.apk
-    camera/basic/build/outputs/apk/debug/basic-debug.apk
-    camera/texture-view/build/outputs/apk/debug/texture-view-debug.apk
-    endless-tunnel/app/build/outputs/apk/debug/app-debug.apk
-    gles3jni/app/build/outputs/apk/debug/app-debug.apk
-    hello-gl2/app/build/outputs/apk/debug/app-debug.apk
-    hello-jni/app/build/outputs/apk/arm7/debug/app-arm7-debug.apk
-    hello-jniCallback/app/build/outputs/apk/debug/app-debug.apk
-    hello-libs/app/build/outputs/apk/debug/app-debug.apk
-    hello-neon/app/build/outputs/apk/debug/app-debug.apk
-    native-activity/app/build/outputs/apk/debug/app-debug.apk
-    native-audio/app/build/outputs/apk/debug/app-debug.apk
-    native-codec/app/build/outputs/apk/debug/app-debug.apk
-    native-media/app/build/outputs/apk/debug/app-debug.apk
-    native-plasma/app/build/outputs/apk/debug/app-debug.apk
-#    other-builds
-    sensor-graph/accelerometer/build/outputs/apk/debug/accelerometer-debug.apk
-    san-angeles/app/build/outputs/apk/debug/app-armeabi-v7a-debug.apk
-    san-angeles/app/build/outputs/apk/debug/app-arm64-v8a-debug.apk
-    san-angeles/app/build/outputs/apk/debug/app-x86-debug.apk
-    teapots/classic-teapot/build/outputs/apk/debug/classic-teapot-debug.apk
-    teapots/more-teapots/build/outputs/apk/debug/more-teapots-debug.apk
+  audio-echo/app/build/outputs/apk/debug/app-debug.apk
+  bitmap-plasma/app/build/outputs/apk/debug/app-debug.apk
+  camera/basic/build/outputs/apk/debug/basic-debug.apk
+  camera/texture-view/build/outputs/apk/debug/texture-view-debug.apk
+  endless-tunnel/app/build/outputs/apk/debug/app-debug.apk
+  gles3jni/app/build/outputs/apk/debug/app-debug.apk
+  hello-gl2/app/build/outputs/apk/debug/app-debug.apk
+  hello-jni/app/build/outputs/apk/arm7/debug/app-arm7-debug.apk
+  hello-jniCallback/app/build/outputs/apk/debug/app-debug.apk
+  hello-libs/app/build/outputs/apk/debug/app-debug.apk
+  hello-neon/app/build/outputs/apk/debug/app-debug.apk
+  native-activity/app/build/outputs/apk/debug/app-debug.apk
+  native-audio/app/build/outputs/apk/debug/app-debug.apk
+  native-codec/app/build/outputs/apk/debug/app-debug.apk
+  native-media/app/build/outputs/apk/debug/app-debug.apk
+  native-plasma/app/build/outputs/apk/debug/app-debug.apk
+  sensor-graph/accelerometer/build/outputs/apk/debug/accelerometer-debug.apk
+  san-angeles/app/build/outputs/apk/debug/app-armeabi-v7a-debug.apk
+  san-angeles/app/build/outputs/apk/debug/app-arm64-v8a-debug.apk
+  san-angeles/app/build/outputs/apk/debug/app-x86-debug.apk
+  teapots/classic-teapot/build/outputs/apk/debug/classic-teapot-debug.apk
+  teapots/more-teapots/build/outputs/apk/debug/more-teapots-debug.apk
 #    webp/view/build/outputs/apk/debug/view-arm7-debug.apk
-    teapots/choreographer-30fps/build/outputs/apk/debug/choreographer-30fps-debug.apk)
+  teapots/choreographer-30fps/build/outputs/apk/debug/choreographer-30fps-debug.apk
+
+## other-builds
+  other-builds/ndkbuild/bitmap-plasma/app/build/outputs/apk/debug/app-debug.apk
+  other-builds/ndkbuild/gles3jni/app/build/outputs/apk/debug/app-debug.apk
+  other-builds/ndkbuild/hello-gl2/app/build/outputs/apk/debug/app-debug.apk
+  other-builds/ndkbuild/hello-jni/app/build/outputs/apk/debug/app-debug.apk
+  other-builds/ndkbuild/hello-libs/app/build/outputs/apk/debug/app-debug.apk
+  other-builds/ndkbuild/hello-neon/app/build/outputs/apk/arm7/debug/app-arm7-debug.apk
+  other-builds/ndkbuild/native-activity/app/build/outputs/apk/debug/app-debug.apk
+  other-builds/ndkbuild/native-audio/app/build/outputs/apk/debug/app-debug.apk
+  other-builds/ndkbuild/native-codec/app/build/outputs/apk/debug/app-debug.apk
+  other-builds/ndkbuild/native-media/app/build/outputs/apk/debug/app-debug.apk
+  other-builds/ndkbuild/native-plasma/app/build/outputs/apk/debug/app-debug.apk
+  other-builds/ndkbuild/nn_sample/app/build/outputs/apk/debug/app-debug.apk
+  other-builds/ndkbuild/san-angeles/app/build/outputs/apk/debug/app-armeabi-v7a-debug.apk
+  other-builds/ndkbuild/san-angeles/app/build/outputs/apk/debug/app-arm64-v8a-debug.apk
+  other-builds/ndkbuild/san-angeles/app/build/outputs/apk/debug/app-x86-debug.apk
+  other-builds/ndkbuild/teapots/more-teapots/build/outputs/apk/debug/more-teapots-debug.apk
+  other-builds/ndkbuild/teapots/classic-teapot/build/outputs/apk/debug/classic-teapot-debug.apk
+)
 
 rm -fr ${BUILD_RESULT_FILE}
 for apk in "${apks[@]}"; do
