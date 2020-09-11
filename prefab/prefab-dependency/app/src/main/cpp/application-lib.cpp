26772fbb207cddf66524d13c2b22ab4bf77b66b5
ru26kif <constantin.geier@campus.lmu.de>
2020-09-12 00:03:08 +0200

    now prefab is working properly. Yeah !

--- prefab/prefab-dependency/app/src/main/cpp/application-lib.cpp
+++ prefab/prefab-dependency/app/src/main/cpp/application-lib.cpp
@@ -1,5 +1,6 @@
 #include <jni.h>
 #include <string>
+#include "mylibrary/mylibrary.h"
 
 extern "C" {
 JNIEXPORT jstring JNICALL
@@ -7,7 +8,8 @@ Java_constantin_prefab_1dependency_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
     // Here I want to use the 'mylibrary'  my_api() method for the string
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+    //std::string hello = "Hello from C++";
+    const std::string stringFromMyLibrary=my_api();
+    return env->NewStringUTF(stringFromMyLibrary.c_str());
 }
 }
