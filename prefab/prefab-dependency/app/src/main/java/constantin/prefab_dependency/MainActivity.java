ef896e73769dccf6626fcb722a89212a0b6ede94
ru26kif <constantin.geier@campus.lmu.de>
2020-09-11 13:49:19 +0200

    Add prefab-dependency as an example project

---
+++ prefab/prefab-dependency/app/src/main/java/constantin/prefab_dependency/MainActivity.java
@@ -0,0 +1,30 @@
+package constantin.prefab_dependency;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+        TextView tv = findViewById(R.id.sample_text);
+        tv.setText(stringFromJNI());
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
