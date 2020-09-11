20c91722b590908315f605786df33a656ce90a94
ru26kif <constantin.geier@campus.lmu.de>
2020-09-11 19:17:00 +0200

    add some documentation to clarify my intensions

--- prefab/prefab-dependency/app/src/main/cpp/application-lib.cpp
+++ prefab/prefab-dependency/app/src/main/cpp/application-lib.cpp
@@ -6,6 +6,7 @@ JNIEXPORT jstring JNICALL
 Java_constantin_prefab_1dependency_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
+    // Here I want to use the 'mylibrary'  my_api() method for the string
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
