456581406fb44af8c80f56b9265a32d2b4fef578
ru26kif <constantin.geier@campus.lmu.de>
2020-09-14 14:16:56 +0200

    update TelemetryCore-debug.aar

--- prefab/prefab-dependency/app/src/main/cpp/application-lib.cpp
+++ prefab/prefab-dependency/app/src/main/cpp/application-lib.cpp
@@ -1,6 +1,7 @@
 #include <jni.h>
 #include <string>
-#include "mylibrary/mylibrary.h"
+//#include "mylibrary/mylibrary.h"
+#include "BaseTelemetryReceiver.h"
 
 extern "C" {
 JNIEXPORT jstring JNICALL
@@ -9,7 +10,10 @@ Java_constantin_prefab_1dependency_MainActivity_stringFromJNI(
         jobject /* this */) {
     // Here I want to use the 'mylibrary'  my_api() method for the string
     //std::string hello = "Hello from C++";
-    const std::string stringFromMyLibrary=my_api();
+    //const std::string stringFromMyLibrary=my_api();
+    BaseTelemetryReceiver* t;
+    const std::string stringFromMyLibrary="";
+    //TelemetryReceiver* telemetryReceiver;
     return env->NewStringUTF(stringFromMyLibrary.c_str());
 }
 }
