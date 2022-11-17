1b21dc39837abe0b1b471d5e300eb184ae97ff47
James Farrell <jamesfarrell@google.com>
2022-11-17 17:20:04 -0600

    Restore "clean" before building.

--- .ci_tools/build_samples.sh
+++ .ci_tools/build_samples.sh
@@ -64,7 +64,7 @@ fi
 for d in "${projects[@]}"; do
     pushd ${NDK_SAMPLE_REPO}/${d} >/dev/null
     echo "Building ${d}"
-    TERM=dumb ./gradlew ${VERBOSITY} assembleDebug
+    TERM=dumb ./gradlew ${VERBOSITY} clean assembleDebug
     popd >/dev/null
 done
 
