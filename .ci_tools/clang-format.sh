e2f9e90d3dc2471016b1c67863e978ad91a9ba83
James Farrell <jamesfarrell@google.com>
2022-12-06 14:24:02 -0600

    Comments.

--- .ci_tools/clang-format.sh
+++ .ci_tools/clang-format.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# Guess ANDROID_HOME if it's not set, and also detect arch.
 case $(uname -s) in
   Linux)
     ANDROID_HOME=${ANDROID_HOME:-${HOME}/Android/Sdk}
@@ -10,5 +11,7 @@ case $(uname -s) in
     ARCH_DIR=darwin-x86_64
 esac
 
+# Guess the latest version of the NDK we are using, and use that everywhere.
 NDK_VERSION=$(find . -name build.gradle | xargs grep -h ndkVersion | sort -n | uniq | cut -d \' -f 2 | tail -1)
+
 ${ANDROID_HOME}/ndk/${NDK_VERSION}/toolchains/llvm/prebuilt/${ARCH_DIR}/bin/clang-format $@
