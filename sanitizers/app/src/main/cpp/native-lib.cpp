c3a68e646dcdb9a12cce38023b64c01ed7f19cc3
James Farrell <jamesfarrell@google.com>
2023-01-18 13:43:11 -0600

    Sanitizer example with build variants for asan and hwasan.

--- sanitizers/app/src/main/cpp/native-lib.cpp
+++ sanitizers/app/src/main/cpp/native-lib.cpp
@@ -5,6 +5,10 @@
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_sanitizers_MainActivity_stringFromJNI(JNIEnv* env,
                                                        jobject /* this */) {
+  int* foo = new int;
+  *foo = 3;
+  delete foo;
+  *foo = 4;
   std::string hello = "Hello from C++";
   return env->NewStringUTF(hello.c_str());
 }
