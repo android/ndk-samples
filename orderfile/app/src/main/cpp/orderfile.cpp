80c27fefc76b2933cd45f34e1b1aa83e352c5b41
Sharjeel Khan <sharjeelkhan@google.com>
2023-08-04 13:26:15 -0700

    Formatted files based on clang-format

--- orderfile/app/src/main/cpp/orderfile.cpp
+++ orderfile/app/src/main/cpp/orderfile.cpp
@@ -1,11 +1,9 @@
+#include <android/log.h>
 #include <errno.h>
+#include <jni.h>
 #include <linux/limits.h>
 #include <stdlib.h>
 #include <string.h>
-
-#include <jni.h>
-
-#include <android/log.h>
 
 const char kLogTag[] = "orderfiledemo";
 
@@ -15,53 +13,42 @@ extern "C" int __llvm_profile_initialize_file(void);
 extern "C" int __llvm_orderfile_dump(void);
 #endif
 
-void DumpProfileDataIfNeeded(const char* temp_dir) {
+void DumpProfileDataIfNeeded(const char *temp_dir) {
 #ifdef GENERATE_PROFILES
-    char profile_location[PATH_MAX] = {};
-    snprintf(profile_location, sizeof(profile_location), "%s/demo.output", temp_dir);
-    if (__llvm_profile_set_filename(profile_location) == -1) {
-        __android_log_print(
-                ANDROID_LOG_ERROR,
-                kLogTag,
-                "__llvm_profile_set_filename(\"%s\") failed: %s",
-                profile_location,
-                strerror(errno));
-        return;
-    }
+  char profile_location[PATH_MAX] = {};
+  snprintf(profile_location, sizeof(profile_location), "%s/demo.output",
+           temp_dir);
+  if (__llvm_profile_set_filename(profile_location) == -1) {
+    __android_log_print(ANDROID_LOG_ERROR, kLogTag,
+                        "__llvm_profile_set_filename(\"%s\") failed: %s",
+                        profile_location, strerror(errno));
+    return;
+  }
 
-    if (__llvm_profile_initialize_file() == -1) {
-            __android_log_print(
-                    ANDROID_LOG_ERROR,
-                    kLogTag,
-                    "__llvm_profile_initialize_file failed: %s",
-                    strerror(errno));
-            return;
-        }
+  if (__llvm_profile_initialize_file() == -1) {
+    __android_log_print(ANDROID_LOG_ERROR, kLogTag,
+                        "__llvm_profile_initialize_file failed: %s",
+                        strerror(errno));
+    return;
+  }
 
-    if (__llvm_orderfile_dump() == -1) {
-        __android_log_print(
-                ANDROID_LOG_ERROR,
-                kLogTag,
-                "__llvm_orderfile_dump() failed: %s",
-                strerror(errno));
-        return;
-    }
-    __android_log_print(
-            ANDROID_LOG_DEBUG,
-            kLogTag,
-            "Wrote profile data to %s",
-            profile_location);
+  if (__llvm_orderfile_dump() == -1) {
+    __android_log_print(ANDROID_LOG_ERROR, kLogTag,
+                        "__llvm_orderfile_dump() failed: %s", strerror(errno));
+    return;
+  }
+  __android_log_print(ANDROID_LOG_DEBUG, kLogTag, "Wrote profile data to %s",
+                      profile_location);
 #else
-    __android_log_print(
-            ANDROID_LOG_DEBUG, kLogTag,
-            "Did not write profile data because the app was not built for profile generation");
+  __android_log_print(ANDROID_LOG_DEBUG, kLogTag,
+                      "Did not write profile data because the app was not "
+                      "built for profile generation");
 #endif
 }
 
 extern "C" JNIEXPORT void JNICALL
-Java_com_example_orderfiledemo_MainActivity_runWorkload(
-        JNIEnv *env,
-        jobject /* this */,
-        jstring temp_dir) {
-    DumpProfileDataIfNeeded(env->GetStringUTFChars(temp_dir, 0));
+Java_com_example_orderfiledemo_MainActivity_runWorkload(JNIEnv *env,
+                                                        jobject /* this */,
+                                                        jstring temp_dir) {
+  DumpProfileDataIfNeeded(env->GetStringUTFChars(temp_dir, 0));
 }
