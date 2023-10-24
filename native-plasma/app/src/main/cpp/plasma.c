62874e3fbd54cb8017f78b1a87c2656f76bb2e08
dimitry <dimitry@google.com>
2023-10-24 16:53:36 +0200

    Changes needed to be able to compile native-plasma with wasm_ndk

--- native-plasma/app/src/main/cpp/plasma.c
+++ native-plasma/app/src/main/cpp/plasma.c
@@ -27,9 +27,9 @@
 #include <time.h>
 
 #define LOG_TAG "libplasma"
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
-#define LOGW(...) __android_log_print(ANDROID_LOG_WARN, LOG_TAG, __VA_ARGS__)
-#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+#define LOGI(...)
+#define LOGW(...)
+#define LOGE(...)
 
 /* Set to 1 to enable debug log traces. */
 #define DEBUG 0
