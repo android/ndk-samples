94bc8223b6e8a04eccd80401326e1dfab1c3ac57
Shahbaz Youssefi <syoussefi@google.com>
2024-04-09 10:09:09 -0400

    Prepare Hello GL for multisampling demo
    Adds a few images and an expensive render pass to draw into them.  A
    part of the image is blit into the swapchain for visual verification.
    This preparation step does the above in single-sampled rendering, with
    follow up changes enabling multisampling.
    
    The goal is to demonstrate how multisampling can be done efficiently
    (by comparing with single-sampled rendering in this commit) on tiling
    GPUs.

--- hello-gl2/app/src/main/cpp/gl_code.cpp
+++ hello-gl2/app/src/main/cpp/gl_code.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-// OpenGL ES 2.0 code
+// OpenGL ES 3.0 demo for efficient multisampling
 
 #include <GLES2/gl2.h>
 #include <GLES2/gl2ext.h>
@@ -23,6 +23,8 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+#include "gl3stub.h"
 
 #define LOG_TAG "libgl2jni"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
@@ -40,15 +42,24 @@ static void checkGlError(const char* op) {
 }
 
 auto gVertexShader =
-    "attribute vec4 vPosition;\n"
+    "#version 300 es\n"
+    "in vec4 vPosition;\n"
     "void main() {\n"
     "  gl_Position = vPosition;\n"
     "}\n";
 
 auto gFragmentShader =
+    "#version 300 es\n"
     "precision mediump float;\n"
+    "layout(location = 0) out vec4 outColor1;\n"
+    "layout(location = 1) out vec4 outColor2;\n"
+    "layout(location = 2) out vec4 outColor3;\n"
+    "layout(location = 3) out vec4 outColor4;\n"
     "void main() {\n"
-    "  gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);\n"
+    "  outColor1 = vec4(1.0, 0.0, 0.0, 1.0);\n"
+    "  outColor2 = vec4(1.0, 0.0, 0.0, 1.0);\n"
+    "  outColor3 = vec4(1.0, 0.0, 0.0, 1.0);\n"
+    "  outColor4 = vec4(1.0, 0.0, 0.0, 1.0);\n"
     "}\n";
 
 GLuint loadShader(GLenum shaderType, const char* pSource) {
@@ -117,7 +128,20 @@ GLuint createProgram(const char* pVertexSource, const char* pFragmentSource) {
 GLuint gProgram;
 GLuint gvPositionHandle;
 
+GLuint gVertexBuffer;
+
+static constexpr uint32_t kImageWidth = 1024;
+static constexpr uint32_t kImageHeight = 2048;
+static constexpr uint32_t kImageCount = 4;
+GLuint gImages[kImageCount];
+GLuint gFramebuffer;
+
+int gWindowWidth = 1, gWindowHeight = 1;
+
 bool setupGraphics(int w, int h) {
+  gWindowWidth = w;
+  gWindowHeight = h;
+
   printGLString("Version", GL_VERSION);
   printGLString("Vendor", GL_VENDOR);
   printGLString("Renderer", GL_RENDERER);
@@ -133,12 +157,45 @@ bool setupGraphics(int w, int h) {
   checkGlError("glGetAttribLocation");
   LOGI("glGetAttribLocation(\"vPosition\") = %d\n", gvPositionHandle);
 
-  glViewport(0, 0, w, h);
+  constexpr float kVertexData[18] = {
+    -0.984375, -0.99609375,
+    -0.99999, -0.98828125,
+    -0.96875, -0.98828125,
+
+    -0.96875, -0.984375,
+    1., -1.,
+    1., 1.,
+
+    -0.96875, -0.984375,
+    -1., 1.,
+    1., 1.,
+  };
+
+  glGenBuffers(1, &gVertexBuffer);
+  glBindBuffer(GL_ARRAY_BUFFER, gVertexBuffer);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(kVertexData), kVertexData, GL_STATIC_DRAW);
+
+  glUseProgram(gProgram);
+  glVertexAttribPointer(gvPositionHandle, 2, GL_FLOAT, GL_FALSE, 0, nullptr);
+  glEnableVertexAttribArray(gvPositionHandle);
+
+  // Set up color attachments and a framebuffer
+  glGenTextures(kImageCount, gImages);
+  glGenFramebuffers(1, &gFramebuffer);
+  glBindFramebuffer(GL_FRAMEBUFFER, gFramebuffer);
+  GLenum enabledBufs[kImageCount];
+  for (uint32_t i = 0; i < kImageCount; ++i) {
+    glBindTexture(GL_TEXTURE_2D, gImages[i]);
+    glTexStorage2D(GL_TEXTURE_2D, 1, GL_RGBA8, kImageWidth, kImageHeight);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0 + i, GL_TEXTURE_2D, gImages[i], 0);
+    enabledBufs[i] = GL_COLOR_ATTACHMENT0 + i;
+  }
+  glDrawBuffers(kImageCount, enabledBufs);
+
+  glViewport(0, 0, kImageWidth, kImageHeight);
   checkGlError("glViewport");
   return true;
 }
-
-const GLfloat gTriangleVertices[] = {0.0f, 0.5f, -0.5f, -0.5f, 0.5f, -0.5f};
 
 void renderFrame() {
   static float grey;
@@ -146,21 +203,27 @@ void renderFrame() {
   if (grey > 1.0f) {
     grey = 0.0f;
   }
+
+  glBindFramebuffer(GL_FRAMEBUFFER, gFramebuffer);
+
+  // Draw.  Clear before the render pass to use the equivalent of Vulkan's
+  // VK_ATTACHMENT_LOAD_OP_CLEAR (and avoid VK_ATTACHMENT_LOAD_OP_LOAD).
   glClearColor(grey, grey, grey, 1.0f);
   checkGlError("glClearColor");
-  glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+  glClear(GL_COLOR_BUFFER_BIT);
   checkGlError("glClear");
 
   glUseProgram(gProgram);
   checkGlError("glUseProgram");
 
-  glVertexAttribPointer(gvPositionHandle, 2, GL_FLOAT, GL_FALSE, 0,
-                        gTriangleVertices);
-  checkGlError("glVertexAttribPointer");
-  glEnableVertexAttribArray(gvPositionHandle);
-  checkGlError("glEnableVertexAttribArray");
-  glDrawArrays(GL_TRIANGLES, 0, 3);
+  glDrawArrays(GL_TRIANGLES, 0, 9);
   checkGlError("glDrawArrays");
+
+  // Blit to the default framebuffer for verification.
+  glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
+  glBlitFramebuffer(0, 0, 16, 16, 0, 0, gWindowWidth, gWindowHeight,
+      GL_COLOR_BUFFER_BIT, GL_NEAREST);
+  checkGlError("glBlitFramebuffer");
 }
 
 extern "C" {
@@ -176,6 +239,7 @@ JNIEXPORT void JNICALL Java_com_android_gl2jni_GL2JNILib_init(JNIEnv* env,
                                                               jobject obj,
                                                               jint width,
                                                               jint height) {
+  gl3stubInit();
   setupGraphics(width, height);
 }
 
--- hello-gl2/app/src/main/java/com/android/gl2jni/GL2JNIView.java
+++ hello-gl2/app/src/main/java/com/android/gl2jni/GL2JNIView.java
@@ -110,9 +110,9 @@ class GL2JNIView extends GLSurfaceView {
     private static class ContextFactory implements GLSurfaceView.EGLContextFactory {
         private static int EGL_CONTEXT_CLIENT_VERSION = 0x3098;
         public EGLContext createContext(EGL10 egl, EGLDisplay display, EGLConfig eglConfig) {
-            Log.w(TAG, "creating OpenGL ES 2.0 context");
+            Log.w(TAG, "creating OpenGL ES 3.0 context");
             checkEglError("Before eglCreateContext", egl);
-            int[] attrib_list = {EGL_CONTEXT_CLIENT_VERSION, 2, EGL10.EGL_NONE };
+            int[] attrib_list = {EGL_CONTEXT_CLIENT_VERSION, 3, EGL10.EGL_NONE };
             EGLContext context = egl.eglCreateContext(display, eglConfig, EGL10.EGL_NO_CONTEXT, attrib_list);
             checkEglError("After eglCreateContext", egl);
             return context;
