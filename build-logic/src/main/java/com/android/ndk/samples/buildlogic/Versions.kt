762ffabc9e690ddfa2f2f460ca57ac0d56b60e55
Dan Albert <danalbert@google.com>
2024-05-07 17:02:04 -0700

    Update convention NDK to latest.

--- build-logic/src/main/java/com/android/ndk/samples/buildlogic/Versions.kt
+++ build-logic/src/main/java/com/android/ndk/samples/buildlogic/Versions.kt
@@ -3,5 +3,5 @@ package com.android.ndk.samples.buildlogic
 object Versions {
     const val COMPILE_SDK = 34
     const val TARGET_SDK = 34
-    const val NDK = "25.1.8937393" // r25b
+    const val NDK = "26.3.11579264" // r26d
 }
