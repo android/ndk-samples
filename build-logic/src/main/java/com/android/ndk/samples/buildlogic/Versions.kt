369dc3df457adf2a0257f2a6989c588594b569e7
Dan Albert <danalbert@google.com>
2024-05-07 17:11:58 -0700

    Move minSdk to convention plugin.

--- build-logic/src/main/java/com/android/ndk/samples/buildlogic/Versions.kt
+++ build-logic/src/main/java/com/android/ndk/samples/buildlogic/Versions.kt
@@ -3,5 +3,6 @@ package com.android.ndk.samples.buildlogic
 object Versions {
     const val COMPILE_SDK = 34
     const val TARGET_SDK = 34
+    const val MIN_SDK = 21
     const val NDK = "26.3.11579264" // r26d
 }
