a24551c9eff91f72e4384b486fc4968dad245991
Dan Albert <danalbert@google.com>
2025-09-17 16:47:29 -0700

    Increase warning level in unit-test.

--- unit-test/app/src/main/cpp/native-lib.cpp
+++ unit-test/app/src/main/cpp/native-lib.cpp
@@ -2,7 +2,7 @@
 
 #include "adder.h"
 
-extern "C" JNIEXPORT jint JNICALL Java_com_example_unittest_MainActivity_add(
-    JNIEnv* env, jobject /* this */, jint a, jint b) {
+extern "C" JNIEXPORT jint JNICALL
+Java_com_example_unittest_MainActivity_add(JNIEnv*, jobject, jint a, jint b) {
   return add((int)a, (int)b);
 }
