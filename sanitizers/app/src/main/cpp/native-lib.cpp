ace46d87b074ab5b56799db62b0d8bc3063ac012
Dan Albert <danalbert@google.com>
2025-09-18 15:33:36 -0700

    Migrate sanitizers to RegisterNatives.

--- sanitizers/app/src/main/cpp/native-lib.cpp
+++ sanitizers/app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,9 @@
+#include <base/macros.h>
 #include <jni.h>
 
 #include <string>
 
-extern "C" JNIEXPORT jstring JNICALL
-Java_com_example_sanitizers_MainActivity_stringFromJNI(JNIEnv* env,
-                                                       jobject /* this */) {
+jstring StringFromJni(JNIEnv* env, jobject /* this */) {
   // Use-after-free error, caught by asan and hwasan.
   int* foo = new int;
   *foo = 3;
@@ -17,4 +16,24 @@ Java_com_example_sanitizers_MainActivity_stringFromJNI(JNIEnv* env,
 
   std::string hello = "Hello from C++";
   return env->NewStringUTF(hello.c_str());
+}
+
+extern "C" JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* _Nonnull vm,
+                                             void* _Nullable) {
+  JNIEnv* env;
+  if (vm->GetEnv(reinterpret_cast<void**>(&env), JNI_VERSION_1_6) != JNI_OK) {
+    return JNI_ERR;
+  }
+
+  jclass c = env->FindClass("com/example/sanitizers/MainActivity");
+  if (c == nullptr) return JNI_ERR;
+
+  static const JNINativeMethod methods[] = {
+      {"stringFromJNI", "()Ljava/lang/String;",
+       reinterpret_cast<void*>(StringFromJni)},
+  };
+  int rc = env->RegisterNatives(c, methods, arraysize(methods));
+  if (rc != JNI_OK) return rc;
+
+  return JNI_VERSION_1_6;
 }
